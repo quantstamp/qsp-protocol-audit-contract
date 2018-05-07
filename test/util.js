701956ba6ced81e865bf1f0371e23f6fb44f01ae
Ed Zulkoski <ezulkosk@gsd.uwaterloo.ca>
2018-05-07 15:55:37 -0400

    QSP-308: added test cases for various audit request orderings

--- test/util.js
+++ test/util.js
@@ -26,6 +26,49 @@ async function assertTxFail (promise) {
   assert.isTrue(txFailed);
 }
 
+function assertEvent({result, name, args}) {
+  assert.equal(result.logs.length, 1);
+  assert.equal(result.logs[0].event, name);
+  args(result.logs[0].args);
+}
+
+function assertEventAtIndex({result, name, args, index}) {
+  assert.equal(result.logs[index].event, name);
+  args(result.logs[index].args);
+}
+
+const AuditState = Object.freeze({
+  None : 0,
+  Queued : 1,
+  Assigned : 2,
+  Completed : 3,
+  Error : 4,
+  Timeout : 5
+});
+
+
+async function balanceOf (user) {
+  return (await quantstamp_token.balanceOf(user)).toNumber();
+}
+
+async function allowance (owner, spender) {
+  return (await quantstamp_token.allowance(owner, spender)).toNumber();
+}
+
+async function getReportUri (requestId) {
+  const reportUriIndex = 8;
+  return (await quantstamp_audit.audits.call(requestId))[reportUriIndex];
+}
+
+async function getOwnerBalance () {
+  return await web3.eth.getBalance(owner);
+}
+
+function extractRequestId(result) {
+  return result.logs[0].args.requestId.toNumber();
+}
+
+
 module.exports = {
   toEther : toEther,
   toQsp : toEther,
@@ -35,5 +78,14 @@ module.exports = {
   tenEther : toEther(10),
   hundredEther : toEther(100),
   expectThrow : expectThrow,
-  assertTxFail : assertTxFail
+  assertTxFail : assertTxFail,
+  assertEvent : assertEvent,
+  assertEventAtIndex : assertEventAtIndex,
+  AuditState : AuditState,
+  balanceOf : balanceOf,
+  allowance : allowance,
+  getReportUri : getReportUri,
+  getOwnerBalance : getOwnerBalance,
+  extractRequestId : extractRequestId
 };
+
