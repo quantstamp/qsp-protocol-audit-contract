a11582316daef85b2faea747ef0aaaa62b9fe649
VajihM <vajih.montaghami@gmail.com>
2018-08-13 14:55:01 -0400

    debug

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
 const networkConfig = require('../truffle.js');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
   deployer.deploy(Migrations)
     .then(() => new Promise(resolve => setTimeout(() => resolve(), networkConfig.networks[network].delayBetweenDeploys)));
 };
