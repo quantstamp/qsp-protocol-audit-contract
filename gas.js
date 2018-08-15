06389b1ba452577c4207dc00893235d19af2f8dc
Kacper Bak <kacper@quantstamp.com>
2018-08-15 09:37:16 -0500

    fixed the gas limit of submitReport(), fixed calculations, price in USD

--- gas.js
+++ gas.js
@@ -4,8 +4,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io
 
 const GasLimitGetNextAuditOK = web3.utils.toBN(171081);
 const GasLimitGetNextAuditFail = web3.utils.toBN(32762);
-const GasLimitSubmitReport = web3.utils.toBN(295406);
-const WhitelistedNode = "0xb41f068d6ff6bc5e873196e4d0daa1aca698c64c";
+const GasLimitSubmitReport = web3.utils.toBN(158151);
 
 // the number of decimals in an ERC20 token
 const Decimals = 18;
@@ -15,14 +14,15 @@ async function fetchGasPriceInWei () {
   return web3.utils.toBN(gasPriceHex.result);
 }
 
-async function fetchQspPriceInWei () {
-  const qspPriceInBtc = (await superagent.get("https://api.coinmarketcap.com/v1/ticker/quantstamp/")).body[0].price_btc;
+async function fetchQspPrice () {
+  const qspInfo = (await superagent.get("https://api.coinmarketcap.com/v1/ticker/quantstamp/")).body[0];
+  const qspPriceInBtc = qspInfo.price_btc;
   const ethPriceInBtc = (await superagent.get("https://api.coinmarketcap.com/v1/ticker/ethereum/")).body[0].price_btc;
   const qspPriceInEth = qspPriceInBtc / ethPriceInBtc;
   const qspPriceInEthString = qspPriceInEth.toString();
   const dotPos = qspPriceInEthString.indexOf('.');
   const trimmedQspPrice = (dotPos !== -1) ? qspPriceInEthString.substr(0, dotPos + Decimals + 1) : qspPriceInEthString;
-  return web3.utils.toBN(web3.utils.toWei(trimmedQspPrice, "ether"));
+  return { wei: web3.utils.toBN(web3.utils.toWei(trimmedQspPrice, "ether")), usd: qspInfo.price_usd };
 }
 
 async function calculate (nodes, gasPrice) {
@@ -33,15 +33,16 @@ async function calculate (nodes, gasPrice) {
   }
   const n = web3.utils.toBN(nodes - 1);
   const minPriceInWeiBN = gasPrice.mul(GasLimitGetNextAuditOK.add(GasLimitSubmitReport).add(GasLimitGetNextAuditFail.mul(n)));
-  const minPriceInWei = minPriceInWeiBN.toNumber();
-  const qspPriceInWei = (await fetchQspPriceInWei()).toNumber();
-  const minPriceInQsp = minPriceInWei / qspPriceInWei;
+  const qspPrice = await fetchQspPrice();
+  const qspPriceInWeiBN = qspPrice.wei;
+  const minPriceInQsp = minPriceInWeiBN.div(qspPriceInWeiBN).toNumber();
   const nSuccessfulAudits = web3.utils.toBN(web3.utils.toWei("1", "ether")).div(minPriceInWeiBN).toNumber();
   console.log("Gas price (from Etherscan):");
-  console.log(`  ${gasPrice.toNumber()} Wei = ${web3.utils.fromWei(gasPrice, 'ether')} ETH`);
+  console.log(`  ${gasPrice.toNumber()} Wei = ${web3.utils.fromWei(gasPrice, 'gwei')} GWei = ${web3.utils.fromWei(gasPrice, 'ether')} ETH`);
   console.log("Min suggested price per audit:");
-  console.log(`  ${minPriceInWei} Wei = ${web3.utils.fromWei(minPriceInWeiBN, 'ether')} ETH`);
-  console.log(`  ${web3.utils.toWei(minPriceInQsp.toString(), "ether")} miniQSP = ${minPriceInQsp} QSP`);
+  console.log(`  ${minPriceInWeiBN.toString()} Wei = ${web3.utils.fromWei(minPriceInWeiBN, 'ether')} ETH`);
+  console.log(`  ${web3.utils.toWei(minPriceInQsp.toString(), "ether")} weiQSP = ${minPriceInQsp} QSP`);
+  console.log(`  ${minPriceInQsp * qspPrice.usd} USD`);
   console.log("For 1 ETH, an operator may get:");
   console.log(`  ${nSuccessfulAudits} successful audits`);
   console.log(`  ${nSuccessfulAudits * (nodes - 1)} failed attempts`);
