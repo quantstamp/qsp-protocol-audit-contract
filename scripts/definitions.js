0a139c715ef4398719f21da1e9317f02f269586c
VajihM <vajih.montaghami@gmail.com>
2018-08-30 18:51:03 -0400

    QSP-638 set min audit price to max uint256

--- scripts/definitions.js
+++ scripts/definitions.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const utils = require('../migrations/utils.js');
+const BN = require('web3').utils.BN;
 
 module.exports = {
   'whitelist': {
@@ -9,6 +10,23 @@ module.exports = {
     gasLimit: 80000,
     methodArgs: async(stage, argv) => {
       return argv.p;
+    }
+  },
+  'whitelist-owner-in-data-contract': {
+    contractName: 'QuantstampAuditData',
+    methodName: 'addAddressToWhitelist',
+    gasLimit: 80000,
+    methodArgs: async(stage, argv) => {
+      return [require('../truffle.js').networks[stage].account];
+    }
+  },
+  'update-min-price-to-max': {
+    contractName: 'QuantstampAuditData',
+    methodName: 'setMinAuditPrice',
+    gasLimit: 80000,
+    methodArgs: async(stage, argv) => {
+      let maxUint256 = new BN(0).notn(256);
+      return [argv.p[0], maxUint256.toString()];
     }
   },
   'whitelist-audit-contract': {
