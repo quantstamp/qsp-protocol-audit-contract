5963e47f9e673cb7ad40f54de1584e12949708c4
VajihM <vajih.montaghami@gmail.com>
2018-10-19 09:45:38 -0400

    fixed issue reported by linter

--- contracts/QuantstampAuditReportData.sol
+++ contracts/QuantstampAuditReportData.sol
@@ -8,7 +8,6 @@ contract QuantstampAuditReportData is Whitelist {
   // mapping from requestId to a report
   mapping(uint256 => bytes1[]) public reports;
 
-
   function setReport(uint256 requestId, bytes1[] report) external onlyWhitelisted {
     reports[requestId] = report;
   }
