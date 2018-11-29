f87d8e1c325731b90726aa0a064d172a0e4de098
arshdeep tinna <astinna@pm.me>
2018-11-29 15:18:50 -0500

    Whitelist audit contract in police

--- scripts/definitions.js
+++ scripts/definitions.js
@@ -75,6 +75,14 @@ module.exports = {
       return [await utils.readAddressFromMetadata(stage, 'QuantstampAuditPolice')];
     }
   },
+  'whitelist-audit-contract-in-police': {
+    contractName: 'QuantstampAuditPolice',
+    methodName: 'addAddressToWhitelist',
+    gasLimit: 80000,
+    methodArgs: async(stage, argv) => {
+      return [await utils.readAddressFromMetadata(stage, 'QuantstampAudit')];
+    }
+  },
   'set-max-assigned': {
     contractName: 'QuantstampAuditData',
     methodName: 'setMaxAssignedRequests',
