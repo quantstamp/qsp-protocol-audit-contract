11f45558bd4b176dcb7a725b0500cdccc033b983
arshdeep tinna <astinna@pm.me>
2019-01-30 11:24:02 -0500

    Get commit hash from meta.json files in aws s3

--- deployer.js
+++ deployer.js
@@ -1,9 +1,31 @@
+// Script to generate contract deployment manifest 
+// To use this script udpate the config in deploy.yml
+// which currently  supports only version and network
+// specifications.
+//
+// Once updated, the following script will check 
+// meta.json files for each contract in the desired
+// environment to find out the last succesfully deployed
+// commit hash. It will then determine the contracts
+// that have been since updated and will update truffle.js
+// file to mark them `true` for deployment
+// 
+// Next it will generate a script deploy-<network>.sh
+// containing truffle migrate and whitelisting command.
+//  As the final step, it will update the version in
+// package.json to version specified in deploy.yml
+// To run the script:
+//     node deployer.js
+
 const fs = require('fs')
 const yaml = require('js-yaml');
 const editJsonFile = require("edit-json-file");
 const shell = require('shelljs');
 const path = require('path');
 const replace = require('replace-in-file');
+const aws = require('aws-sdk');
+const s3 = new aws.S3(); 
+const definitions = require('./scripts/definitions.js')
 
 function getConfig() {
 try {
@@ -15,26 +37,47 @@ try {
   }
 }
 
+function getCurrentVersion() {
+    var content = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+    return content.version
+}
+
+function getAllContractNames() {
+    var files = fs.readdirSync('./contracts/').filter(file => file.startsWith('Quantstamp'))
+    files = files.map(file => file.split('.')[0])
+    return files
+}
+
 function updateVersion(config) {
     let packageJson = editJsonFile('package.json')
     let version = packageJson.get("version")
-    if (!(version >= config.deploy.version)) {
+    if (config.deploy.version >= version) {
         packageJson.set("version", config.deploy.version)
         packageJson.save()
-        console.log("Version updated to " + config.deploy.version)
-    } else throw Error("New version number should be greater than current version")
+        console.log("Version in package.json updated to " + config.deploy.version)
+    } else throw Error("New version number should be greater than or equal to current version")
 }
 
-function getCommitHash() {
-    let hash = shell.exec('git rev-parse HEAD', {silent:true}).stdout;
-    console.log("Commit Hash is " + hash)
-    return  hash
+async function getCommitHash(currentVersion, network, contract) {
+    let majorVersion = currentVersion.split('.')[0]
+    var commitHash = null
+    if (network === 'development') {
+        commitHash = shell.exec('git rev-parse HEAD', {silent:true}).stdout;
+        return  commitHash
+    }
+    var getParams = {
+        Bucket: 'qsp-protocol-contract',
+        Key: network + '/' + contract + '-v-' + majorVersion + "-meta.json"
+    }
+    //console.log(getParams)
+    var response = await s3.getObject(getParams).promise().catch(err => console.log(err))
+    var data = response.Body.toString()
+    commitHash = JSON.parse(data).commitHash
+    return commitHash
 }
 
-function getDiffFiles() {
-    let commitHash = getCommitHash()
-    files = shell.exec('git diff-tree --name-only --no-commit-id -r ' + commitHash, {silent:true}).stdout.split("\n");
-    // console.log(files)
+function getDiffFiles(commitHash) {
+    files = shell.exec('git diff --name-only HEAD ' + commitHash, {silent:true}).stdout.split("\n");
     return files
 }
 
@@ -42,7 +85,7 @@ function getUpdatedContractNames(fileNames) {
     let sol = new RegExp('.sol$')
     let files = fileNames.filter(fileName => fileName.match(sol))
     let contractNames = files.map(file => path.parse(file).base.split(".")[0])
-    console.log("Updated contract names are: " + contractNames)
+    console.log("Found updated contracts")
     return contractNames
 }
 
@@ -82,22 +125,26 @@ function writeTruffleCommands(network, deployScript) {
     }
 }
 
-function findWhiteListCommands(contractNames) {
+function findWhiteListCommands(updatedContractNames) {
     let whitelistDefs = []
-    let definitions = fs.readFileSync('scripts/definitions.js', 'utf8')
-    // console.log(definitions)
-    contractNames.forEach(contract => {
-        r = '\'(whitelist.*)\':\\s*{\\s*contractName:\\s*\'' + contract + '\',\\s*methodName:\\s*\'addAddressToWhitelist\''
-        f = new RegExp(r, 'g')  
-        // console.log(f)
-        do {
-            matches = f.exec(definitions)
-            //console.log(matches)
-            if (matches !== null && matches.length == 2) {
-                whitelistDefs.push(matches[1])
+    defKeys = Object.keys(definitions)
+    updatedContractNames.forEach(updatedContractName => {
+        defKeys.forEach(key => {
+            // console.log(key)
+            method = definitions[key]["methodName"]
+            if (method === 'addAddressToWhitelist') { 
+                Contract = definitions[key]["contractName"]
+                asynDef = definitions[key]["methodArgs"].toString()
+                // console.log(contract)
+                // console.log(asynDef)
+                if (contract === updatedContractName || asynDef.includes(updatedContractName)) {
+                    whitelistDefs.push(key)
+                }
             }
-        } while (matches);
+        })
     })
+    whitelistDefs = [...new Set(whitelistDefs)]
+    // console.log(whitelistDefs)
     return whitelistDefs
 }
 
@@ -106,7 +153,7 @@ function writeWhiteListcommands(network, whitelistDefs, deployScript) {
         content = "npm run command -- -n="  + network+ " -a=" + whitelistDef + "\n"
         try {
             fs.writeFileSync(deployScript, content, {mode: '744', flag: 'a'})
-            console.log("Wrote whitelist command for " + whitelistDef + " to "+  deployScript)
+            console.log("=======\nWrote whitelist command for " + whitelistDef + " to "+  deployScript)
         } catch(err) {
             // undoAllChanges()
             throw err
@@ -114,27 +161,44 @@ function writeWhiteListcommands(network, whitelistDefs, deployScript) {
     })
 }
 
-let config = getConfig()
-if (!config) {
-    process.exit()
+function main() {
+    let config = getConfig()
+    if (!config) {
+        process.exit()
+    }
+
+    let allContracts = getAllContractNames()
+
+    config.deploy.network.forEach(async (network) => {
+        var updatedContractNames = []
+        var currentVersion = getCurrentVersion()
+        var deployScript = "deploy-" + network.name + ".sh"
+
+        writeTruffleCommands(network.name, deployScript)
+
+        for (i = 0; i < allContracts.length; i++) {
+            contract = allContracts[i]
+            let commitHash = await getCommitHash(currentVersion, network.name, contract)
+            console.log("=======\nChecking commit hash for " + contract + " for version: " + currentVersion)
+            console.log("Commit Hash is " + commitHash)
+            if (commitHash !== null) {
+                let fileNames = getDiffFiles(commitHash)
+                updatedContractNames = updatedContractNames.concat(getUpdatedContractNames(fileNames))
+            } else {
+                console.log("Could not find a commit hash for current major version")
+            }
+        }
+    
+        updatedContractNames = [...new Set(updatedContractNames)]
+        console.log("=======\nUpdated contracts are: " + updatedContractNames)
+        updateTruffle(updatedContractNames)
+        var whitelistDefs = findWhiteListCommands(updatedContractNames)
+        if (whitelistDefs.length > 0) {
+            console.log("=======\nFound following matching whitelisting definitions: " + whitelistDefs)
+        }
+        writeWhiteListcommands(network.name, whitelistDefs, deployScript) 
+    })
+    updateVersion(config)
 }
 
-let fileNames = getDiffFiles()
-let contractNames = getUpdatedContractNames(fileNames)
-// console.log(contractNames)
-updateVersion(config)
-updateTruffle(contractNames)
-console.log(config.deploy.network)
-config.deploy.network.forEach(network => {
-
-    console.log(network.name)
-    let deployScript = "deploy-" + network.name + ".sh"
-    writeTruffleCommands(network.name, deployScript)
-    let whitelistDefs = findWhiteListCommands(contractNames)
-    if (whitelistDefs.length > 0) {
-        console.log("Found following matching whitelisting definitions: " + whitelistDefs)
-    }
-    writeWhiteListcommands(network.name, whitelistDefs, deployScript) 
-})
-
-
+main()
