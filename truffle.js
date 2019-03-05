9e3e42a41d25e1a5ffad1bbcf47f8196e126bbef
Arshdeep tinna <arshdeep.sm4@gmail.com>
2019-03-05 17:53:06 -0500

    Qsp 1025 separate testnet (#102)
    * QSP-1025 create separate testnet for smart contracts
    
    * Increase whitelist police node gas limit
    
    * Fix identation

--- truffle.js
+++ truffle.js
@@ -39,6 +39,16 @@ module.exports = {
       gasPrice: 110000000000,
       account: '0x0283c049ed4705e2d98c807dbafdaf725f34b8d2'
     },
+    testnet: {
+      provider:  function() {
+        return new HDWalletProvider(credentials.mnemonic, `https://ropsten.infura.io/${credentials.infura_apikey}`);
+      },
+      network_id: 3,
+      gas: 6712388,
+      gasPrice: 110000000000,
+      account: '0x0283c049ed4705e2d98c807dbafdaf725f34b8d2',
+      skipDryRun: true
+    },
     staging: {
       provider:  function() {
         return new HDWalletProvider(credentials.mnemonic, `https://ropsten.infura.io/${credentials.infura_apikey}`);
