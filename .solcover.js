0423962717067b611542a16ef5f8fbe6f775c979
arshdeep tinna <astinna@pm.me>
2019-04-26 13:29:07 -0400

    run single test to reduce the stdout size for coveralls

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: 7545,
     compileCommand: "cp ../truffle-coverage.js ./truffle.js && truffle compile",
-    testCommand: "cp ../truffle-coverage.js ./truffle.js && truffle test",
+    testCommand: "cp ../truffle-coverage.js ./truffle.js && truffle test ./test/quantstamp_audit_expired.js",
     copyPackages: ['openzeppelin-solidity'],
     skipFiles: ['test/QuantstampToken.sol', 'LinkedListLib.sol']
 };
