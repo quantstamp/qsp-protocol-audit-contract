cfc0414243592b61f9ebda5fa88554e1ff8348cf
alex-murashkin <alexmur90@gmail.com>
2019-04-29 16:19:10 -0400

    Minor fixes

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
-  infura_apikey : "d1c018fee4ed42f3b0f8d82743aff90b",
-  mnemonic : "mango wool squeeze giggle creek assault tumble loan point oval youth square"
+  infura_apikey : "",
+  mnemonic : ""
 };
